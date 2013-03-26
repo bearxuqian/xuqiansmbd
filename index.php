f7e65fbf57536dd518720a31373681179554348b
bear <bearxuqian@gmail.com>
2013-03-26 15:14:17 +0800

    init

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+	<!--html注释-->
+	<title>PHP视频学习</title>
+</head>
+<body>
+<?php
+	//单行php注释
+	#单行			1.8E+308 = 1.8*10的308次方
+	/*多行注释*/
+	//自定义常量	bool define(string $name, mixed $value[,bool case_$insensitive])
+	echo __FILE__;		//系统常量
+	echo __LINE__;
+	echo "Hello World欢迎! ";
+?>
+</body>
+</html>
---
+++ test.php
@@ -0,0 +1,6 @@
+<?php
+	//array(value)/array(key=>value)
+	$arr = array(11,22,33,44);
+	unset($arr[0]);
+	echo $arr[1];
+?>
